0f2d9d7a13c546c0f8a47079447ee46145cfdfc7
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-09-20 11:52:02 -0700

    display player from state HP alive/dead and add more assets and make render and utils js files

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 /* Imports */
 
 /* Get DOM Elements */
+const harryHp = document.getElementById('hphp-span');
+const playerImage = document.getElementById('player-image');
 
 /* State */
+let playerHealth = 100;
 
 /* Events */
 
 /* Display Functions */
+function displayPlayer() {
+    harryHp.textContent = playerHealth;
+    if (playerHealth < 1) {
+        playerImage.src = 'assets/ghost.png';
+    } else {
+        playerImage.src = 'assets/harry-potter-round.png';
+    }
+}
 
 // (don't forget to call any display functions you want to run on page load!)
+displayPlayer();
