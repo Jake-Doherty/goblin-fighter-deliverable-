965a851593e7bcc87dc3c31a85c6a94ca6de450e
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-09-20 12:23:22 -0700

    display current message from state and fix/add a little more CSS

--- app.js
+++ app.js
@@ -3,13 +3,19 @@
 /* Get DOM Elements */
 const harryHp = document.getElementById('hphp-span');
 const playerImage = document.getElementById('player-image');
+const battleMessage = document.getElementById('battle-result-message');
 
 /* State */
 let playerHealth = 100;
+let result = 'Click on a creature to get started!';
 
 /* Events */
 
 /* Display Functions */
+function displayResults() {
+    battleMessage.textContent = result;
+}
+
 function displayPlayer() {
     harryHp.textContent = playerHealth;
     if (playerHealth < 1) {
@@ -21,3 +27,4 @@ function displayPlayer() {
 
 // (don't forget to call any display functions you want to run on page load!)
 displayPlayer();
+displayResults();
