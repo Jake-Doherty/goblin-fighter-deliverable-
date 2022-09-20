9785511f4939e9198e0cfe9779589422281bd743
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-09-20 12:48:33 -0700

    displayScoreboard now works from state change scoreboard html structure and fix a few styles

--- app.js
+++ app.js
@@ -4,14 +4,23 @@
 const harryHp = document.getElementById('hphp-span');
 const playerImage = document.getElementById('player-image');
 const battleMessage = document.getElementById('battle-result-message');
+const creaturesCrushed = document.getElementById('creatures-crushed');
+const score = document.getElementById('score');
 
 /* State */
 let playerHealth = 100;
 let result = 'Click on a creature to get started!';
+let totalCreaturesCrushed = 5;
+let playerScore = 10000;
 
 /* Events */
 
 /* Display Functions */
+function displayScoreboard() {
+    creaturesCrushed.textContent = totalCreaturesCrushed;
+    score.textContent = playerScore;
+}
+
 function displayResults() {
     battleMessage.textContent = result;
 }
@@ -28,3 +37,4 @@ function displayPlayer() {
 // (don't forget to call any display functions you want to run on page load!)
 displayPlayer();
 displayResults();
+displayScoreboard();
