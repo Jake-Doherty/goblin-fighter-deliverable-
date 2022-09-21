8e2858f02945d51608d3bdb21a29beff10881bf9
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-09-20 17:38:41 -0700

    display creatures from state name HP alive/dead create render function as well

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 /* Imports */
 
+import { renderCreature } from './render-creature.js';
+
 /* Get DOM Elements */
 const harryHp = document.getElementById('hphp-span');
 const playerImage = document.getElementById('player-image');
 const battleMessage = document.getElementById('battle-result-message');
 const creaturesCrushed = document.getElementById('creatures-crushed');
 const score = document.getElementById('score');
+const creatureList = document.getElementById('creatures-list');
 
 /* State */
 let playerHealth = 100;
 let result = 'Click on a creature to get started!';
 let totalCreaturesCrushed = 5;
 let playerScore = 10000;
+let creatures = [
+    { name: 'Norbert', type: 'dragon', hp: 6 },
+    { name: 'Aragog', type: 'spider', hp: 5 },
+    { name: 'Fenrir', type: 'werewolf', hp: 4 },
+    { name: 'Inferi', type: 'inferi', hp: 0 },
+];
 
 /* Events */
 
@@ -34,7 +43,20 @@ function displayPlayer() {
     }
 }
 
+function displayCreatures() {
+    creatureList.innerHTML = '';
+
+    for (const creature of creatures) {
+        const creatureEl = renderCreature(creature);
+
+        // put event listener in here
+
+        creatureList.append(creatureEl);
+    }
+}
+
 // (don't forget to call any display functions you want to run on page load!)
 displayPlayer();
 displayResults();
 displayScoreboard();
+displayCreatures();
